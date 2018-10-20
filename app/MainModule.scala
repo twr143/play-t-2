3a9f2a8b5bddbc085682e6d0efc11cab5ec0c6bf
twr143 <twr143@gmail.com>
2018-10-20 14:00:51 +0300

    actions chaining, removed origins, 1

--- app/MainModule.scala
+++ app/MainModule.scala
@@ -12,14 +12,6 @@ import services.{ActService1, Service1}
   * Created by Ilya Volynin on 19.10.2018 at 14:29.
   */
 object MainModule {
-
-  val defaultOrigins = Seq(
-    "https://www.dev.raiffeisen.ru", "https://www.test.rbo.raiffeisen.ru",
-    "https://www.rbo.raiffeisen.ru", "https://www.load.rbo.raiffeisen.ru",
-    "https://www.preview.rbo.raiffeisen.ru", "https://www.test.elbrus-ng.raiffeisen.ru",
-    "http://www.dev.raiffeisen.ru", "http://www.test.rbo.raiffeisen.ru",
-    "http://www.load.rbo.raiffeisen.ru", "http://www.test.elbrus-ng.raiffeisen.ru", "https://cpwww.dev.rbp.raiffeisen.ru"
-  )
 }
 
 class MainModule(environment: Environment, configuration: Config) extends AbstractModule with AkkaGuiceSupport with ScalaModule {
