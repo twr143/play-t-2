b45ec4968a8d275f1e08ecabdc2c94df19e30209
twr143 <twr143@gmail.com>
2018-10-26 08:18:41 +0300

    code formatting, 1

--- app/actions/ValidateParamsAction.scala
+++ app/actions/ValidateParamsAction.scala
@@ -1,7 +1,6 @@
 package actions
 import javax.inject.Inject
 import play.api.mvc._
-
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
 
@@ -12,22 +11,19 @@ class ValidateParamsAction @Inject()(defaultParser: BodyParsers.Default)(implici
 
   override def parser: BodyParser[AnyContent] = defaultParser
 
-  override def invokeBlock[A](request: Request[A], block: Request[A] => Future[Result]): Future[Result] =
-  {
+  override def invokeBlock[A](request: Request[A], block: Request[A] => Future[Result]): Future[Result] = {
     val optParam1 = request.getQueryString("param1")
-    if (optParam1.exists{ x=> Try(x.toInt).isFailure})
+    if (optParam1.exists { x => Try(x.toInt).isFailure })
       Future.successful(
-              play.api.mvc.Results.Ok(views.html.error("param1 is NAN")))
-    else if (optParam1.exists{ p => p.toInt<=5 &&p.toInt>=0}) block(request)
-    else if (optParam1.exists{ _.toInt>5})
-      Future.successful(
-        play.api.mvc.Results.Ok(views.html.error("param1 is greater than 5")))
-    else if (optParam1.exists{ _.toInt<0})
-      Future.successful(
-        play.api.mvc.Results.Ok(views.html.error("param1 is negative")))
-    else Future.successful(
-      play.api.mvc.Results.Ok(views.html.error("param1 is not set")))
-
+        play.api.mvc.Results.Ok(views.html.error("param1 is NAN")))
+    else if (optParam1.exists (p => p.toInt <= 5 && p.toInt >= 0 )) block(request)
+    else if (optParam1.exists(_.toInt > 5))
+      Future.successful(play.api.mvc.Results.Ok(views.html.error("param1 is greater than 5")))
+    else if (optParam1.exists {
+      _.toInt < 0
+    })
+      Future.successful(play.api.mvc.Results.Ok(views.html.error("param1 is negative")))
+    else Future.successful(play.api.mvc.Results.Ok(views.html.error("param1 is not set")))
   }
 
   override protected def executionContext: ExecutionContext = ec
