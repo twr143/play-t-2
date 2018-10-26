52f81ae66370b1c26b4cfed85fb045dd6cee7127
twr143 <twr143@gmail.com>
2018-10-26 08:13:45 +0300

    configuration, 1

--- app/MainModule.scala
+++ app/MainModule.scala
@@ -1,9 +1,9 @@
 import actions.ValidateParamsOddEvenAction.SecuredRequest
 import actions.{ValidateParamsAction, ValidateParamsOddEvenAction, ValidateParamsOddEvenActionSecured}
 import com.google.inject.{AbstractModule, TypeLiteral}
-import com.typesafe.config.Config
-import play.api.libs
-import play.Environment
+import javax.inject.Inject
+import play.api.Configuration
+import play.api.Environment
 import net.codingwell.scalaguice._
 import play.api.libs.concurrent.AkkaGuiceSupport
 import services.{ActService1, Service1}
@@ -14,16 +14,16 @@ import services.{ActService1, Service1}
 object MainModule {
 }
 
-class MainModule(environment: Environment, configuration: Config) extends AbstractModule with AkkaGuiceSupport with ScalaModule {
+class MainModule@Inject()(environment: Environment, configuration: Configuration) extends AbstractModule with AkkaGuiceSupport with ScalaModule {
   import MainModule._
 
   override def configure(): Unit = {
-    val addGreet = configuration.getString("user.addtitionalgreet")
-    val showAddGreet = configuration.getBoolean("user.showAdditionalGreet")
+    val addGreet = configuration.get[String]("user.addtitionalgreet")
+    val showAddGreet = configuration.get[Boolean]("user.showAdditionalGreet")
     bind[Boolean].annotatedWithName("showAG").toInstance(showAddGreet)
     bind[String].annotatedWithName("aG").toInstance(addGreet)
     bind[Service1].asEagerSingleton()
-    bind(new TypeLiteral[ValidateParamsOddEvenAction[SecuredRequest]]{}).to(classOf[ValidateParamsOddEvenActionSecured])
+    bind(new TypeLiteral[ValidateParamsOddEvenAction[SecuredRequest]] {}).to(classOf[ValidateParamsOddEvenActionSecured])
     bind[ValidateParamsAction].asEagerSingleton()
     bindActor[ActService1]("act1")
   }
