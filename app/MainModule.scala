4bffae7cba9564fa1c6b8f09d27c270ef753de10
AlexSmith1988 <3032303Abc>
2018-10-28 20:15:23 +0300

    Basic sorting out

--- app/MainModule.scala
+++ app/MainModule.scala
@@ -1,30 +1,34 @@
+import services.ActorNames.greeter
 import actions.ValidateParamsOddEvenAction.SecuredRequest
 import actions.{ValidateParamsAction, ValidateParamsOddEvenAction, ValidateParamsOddEvenActionSecured}
-import com.google.inject.{AbstractModule, TypeLiteral}
-import javax.inject.Inject
-import play.api.Configuration
-import play.api.Environment
+import com.google.inject.TypeLiteral
 import net.codingwell.scalaguice._
 import play.api.libs.concurrent.AkkaGuiceSupport
-import services.{ActService1, Service1}
+import play.api.{Configuration, Environment}
+import services.{Greeter, NameService}
 
 /**
   * Created by Ilya Volynin on 19.10.2018 at 14:29.
   */
-object MainModule {
-}
-
-class MainModule(environment: Environment, configuration: Configuration) extends AbstractModule with AkkaGuiceSupport with ScalaModule {
-  import MainModule._
+case class MainModule(environment: Environment, configuration: Configuration)
+  extends ScalaModule with AkkaGuiceSupport {
 
   override def configure(): Unit = {
-    val addGreet = configuration.get[String]("user.addtitionalgreet")
-    val showAddGreet = configuration.get[Boolean]("user.showAdditionalGreet")
-    bind[Boolean].annotatedWithName("showAG").toInstance(showAddGreet)
-    bind[String].annotatedWithName("aG").toInstance(addGreet)
-    bind[Service1].asEagerSingleton()
-    bind(new TypeLiteral[ValidateParamsOddEvenAction[SecuredRequest]] {}).to(classOf[ValidateParamsOddEvenActionSecured])
+    bind[Boolean].annotatedWithName("showAG")
+      .toInstance(configuration.get[Boolean]("user.showAdditionalGreet"))
+    bind[String].annotatedWithName("aG")
+      .toInstance(configuration.get[String]("user.addtitionalgreet"))
+
+    bind[NameService].asEagerSingleton()
+
+    bind(new TypeLiteral[ValidateParamsOddEvenAction[SecuredRequest]] {})
+      .to(classOf[ValidateParamsOddEvenActionSecured])
+
     bind[ValidateParamsAction].asEagerSingleton()
-    bindActor[ActService1]("act1")
+    bindActor[Greeter](greeter)
   }
 }
+
+
+
+
